cd683cf95f1db0e6e7ffa738cf7be16f3b6e03ba
noriandadmse-svg <noriandadmse@gmail.com>
2026-06-28 08:01:36 -0300

    Update config.js

--- platform/platform/config.js
+++ platform/platform/config.js
@@ -3,12 +3,4 @@
 //  عدّل هنا فقط إذا تغير المشروع
 // ============================================================
 
-export const firebaseConfig = {
-  apiKey:            "AIzaSyBAi93kthDWgqgilPy-dua56",
-  authDomain:        "adamitii.firebaseapp.com",
-  projectId:         "adamitii",
-  storageBucket:     "adamitii.firebasestorage.app",
-  messagingSenderId: "782842731600",
-  appId:             "1:782842731600:web:3793e8c0131",
-  measurementId:     "G-N0GC988RFY"
-};
+export const firebaseConfig = 
